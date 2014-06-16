9df83deac7ae418b54826414bef86e0f1e6e3d13
think201 <think201@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-16 20:00:24 +0000

    Made the UI/UX more intuitive
    git-svn-id: https://plugins.svn.wordpress.org/get-in-touch/tags/1.0.2@933243 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/colorpicker.js
@@ -0,0 +1,8 @@
+/******* Jquery No Conflict Function *******/
+window.$ = jQuery.noConflict();
+
+$(function(){
+	$(".user_up_button_color").wpColorPicker();
+	$("#git_map_color").wpColorPicker();
+	$("#user_button_color").wpColorPicker();	
+});
